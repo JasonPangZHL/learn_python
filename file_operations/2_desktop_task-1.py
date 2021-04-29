884d87b85cb4fd0b190ab496a5d15b110a995e19
Jason <pangzhl1988@163.com>
2021-04-29 19:31:10 +0800

    update file operations

--- file_operations/2_desktop_task-1.py
+++ file_operations/2_desktop_task-1.py
@@ -2,28 +2,56 @@ from os import walk, mkdir, remove
 import os
 from shutil import copy, move
 
-# 桌面的数据整理，基于名字
-path = 'file_path'  # os.walk 返回 文件路径 文件夹 文件
+#桌面的数据整理 基于名字
+path = 'file_prac' #os.walk 返回 文件路径 文件夹 文件
 for filepath, folders, files in walk(path):
-    print(filepath, files,)
+    # print(filepath, files,)
     for file in files:
-        if '.docx' in file:
+
+        if 'docx' in file:
+            print(os.path.join(filepath,file)) #r'c:\aa\aa.jpg'  'c:\\aa\\aa.jpg' /aaa/aa.jpg
+            copy(os.path.join(filepath,file),os.path.join(r'backupfoldder\docx', file))
+           #move
+        elif 'ppt' in file:
             print(os.path.join(filepath, file))
-            copy(os.path.join(filepath, file), os.path.join(r'backup_folder\docx', file))
-            move(os.path.join(filepath, file), os.path.join(r'backup_folder\docx', file))
-        elif '.ppt' in file:
-            print(os.path.join(filepath, file))
-            copy(os.path.join(filepath, file), os.path.join(r'backup_folder\ppt', file))
-            move(os.path.join(filepath, file), os.path.join(r'backup_folder\ppt', file))
-        elif '.txt' in file:
-            if os.path.exists(r'backup_folder\txt'):
-                print(os.path.join(filepath, file))
-                copy(os.path.join(filepath, file), os.path.join(r'backup_folder\txt', file))
-                move(os.path.join(filepath, file), os.path.join(r'backup_folder\txt', file))
-            else:
-                print('txt is not existed and create folder')
-                os.mkdir(r'backup_folder\txt')
+            copy(os.path.join(filepath, file), os.path.join(r'backupfoldder\ppt', file))
+           #move
+        elif 'txt' in file:
+            if os.path.exists(r'backupfoldder\txt'):
 
                 print(os.path.join(filepath, file))
-                copy(os.path.join(filepath, file), os.path.join(r'backup_folder\txt', file))
-                move(os.path.join(filepath, file), os.path.join(r'backup_folder\txt', file))
+                copy(os.path.join(filepath, file), os.path.join(r'backupfoldder\txt', file))
+
+            else:
+                print('txt is not existed and create folder')
+                os.mkdir(r'backupfoldder\txt')
+
+                print(os.path.join(filepath, file))
+                copy(os.path.join(filepath, file), os.path.join(r'backupfoldder\txt', file))
+
+#压缩处理
+
+
+
+import zipfile
+def zipf():
+    z = zipfile.ZipFile( 'abc.zip','w' )
+    for file in os.listdir('zipfolder'):
+        z.write(os.path.join('zipfolder',file))
+        with open('abc.txt','a',encoding='gbk') as w: #注意编码问题
+            w.write(file+'\n') #
+    z.close()
+
+# zipf()
+
+
+def unzipf():
+    f = zipfile.ZipFile('abc.zip', 'r')
+    for file in f.namelist():
+        f.extract(file, "MYunzip/")
+unzipf()
+
+
+
+
+
--- file_operations/5_pet_task.py
+++ file_operations/5_pet_task.py
@@ -1,17 +1,18 @@
-from os import walk, mkdir, rename
+from os import walk,mkdir,rename,path
 import os
-# from shutil import copy, move
-
-# 以文件夹的名字给宠物命名
-path = 'pets'  # os.walk 返回 文件路径 文件夹 文件
+from shutil import copy, move
+#以文件夹的名字给宠物命名
+path = 'pets' #os.walk 返回 文件路径 文件夹 文件
 for filepath, folders, files in walk(path):
-    print(filepath, folders, files)
-    # 注意我们需要以文件夹名字 作为 文件夹下文件的名字，同时加上一个顺序
+    print(filepath,folders,files)
+    #注意我们需要以文件夹名字 作为 文件夹下文件的名字，同时加上一个顺序
     counter = 0
+    print(filepath)
     for file in files:
         print(filepath)
-        type_name = filepath.split('\\')[-1]  # 注意使用这种方式取 文件夹的名字split
-        renamed = type_name + str(counter) + '.jpg'  # 组合新名字
-        # 注意这里为全路径 源文件 目标文件
-        rename(os.path.join(filepath, file), os.path.join(filepath, renamed))
+        type_name = filepath.split('\\')[-1]  #注意使用这种方式取 文件夹的名字 split
+                    #德牧2.jpg
+        renamed = type_name+str(counter)+'.jpg' #组合新名字
+        #注意这里为全路径 源文件 目标文件
+        rename(os.path.join(filepath,file),os.path.join(filepath,renamed))
         counter += 1
